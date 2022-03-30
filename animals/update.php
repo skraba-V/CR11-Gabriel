e23bc09d7421740a611f4983d59da6d5a33dce2f
skraba-V <47357688+skraba-V@users.noreply.github.com>
2022-03-30 09:59:33 +0200

    Update update.php

--- animals/update.php
+++ animals/update.php
@@ -41,7 +41,33 @@ if ($_GET['id']) {
         
     </head>
     <body class="bbc">
-        <?php require_once '../components/navigation.php' ?>
+    <nav class="navbar navbar-expand-lg navbar-light lit" style="padding-inline:4rem;">
+      <div class="container-fluid">
+          <a class="navbar-brand" href="../home.php">
+            <img src="https://i0.wp.com/ebsalter.com/wp-content/uploads/revslider/slide-3/lion.png?ssl=1" alt="" width="40" height="40" class="d-inline-block align-text-top">
+          
+          </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class=" collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <a class="nav-link" aria-current="page" href="../home.php">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="create.php">hf</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="../logout.php?logout">Sign Out</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="../login.php">Login</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
     
         <div class="container dit">
             <div class="row text-center justify-content-center">
